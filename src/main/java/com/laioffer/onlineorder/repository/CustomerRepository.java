e5d0763d5fd9d8befd28f1c2cb101a542d63f3fc
tonyzx0816 <lizhongxian23@gmail.com>
2026-04-03 16:06:00 -0700

    implemented customer repository for accessing db

--- src/main/java/com/laioffer/onlineorder/repository/CustomerRepository.java
+++ src/main/java/com/laioffer/onlineorder/repository/CustomerRepository.java
@@ -1,8 +1,18 @@
 package com.laioffer.onlineorder.repository;
 
 import com.laioffer.onlineorder.entity.CustomerEntity;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface CustomerRepository extends ListCrudRepository<CustomerEntity, Long> {
+import java.util.List;
 
+public interface CustomerRepository extends ListCrudRepository<CustomerEntity, Long> {
+    List<CustomerEntity> findByFirstName(String firstName);
+    List<CustomerEntity> findByLastName(String lastName);
+    CustomerEntity findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE customer SET first_name = :firstName, last_name = :lastName WHERE email = :email")
+    void updateCustomer(String firstName, String lastName, String email);
 }

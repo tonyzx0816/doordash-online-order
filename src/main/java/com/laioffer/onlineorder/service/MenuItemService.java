50b3531c6f1c78a971bb087fb6e16435c49a0191
tonyzx0816 <lizhongxian23@gmail.com>
2026-04-06 08:57:49 -0700

    implemented MenuItemService to get MenuItemEntity

---
+++ src/main/java/com/laioffer/onlineorder/service/MenuItemService.java
@@ -0,0 +1,24 @@
+package com.laioffer.onlineorder.service;
+
+import com.laioffer.onlineorder.entity.MenuItemEntity;
+import com.laioffer.onlineorder.repository.MenuItemRepository;
+
+import java.util.List;
+
+public class MenuItemService {
+   private final MenuItemRepository menuItemRepository;
+
+   public MenuItemService(MenuItemRepository menuItemRepository) {
+       this.menuItemRepository = menuItemRepository;
+   }
+
+   // MenuItem is for the use of internal or for restaurantService,
+   // so it doesn't need to convert to Dto, provide more flexibility
+   public List<MenuItemEntity> getMenuItemsByRestaurantId(long restaurantId){
+       return menuItemRepository.getByRestaurantId(restaurantId);
+   }
+
+   public MenuItemEntity getMenuItemById(long id){
+       return menuItemRepository.findById(id).get();
+   }
+}

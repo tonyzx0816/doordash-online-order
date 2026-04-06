569f48cb3604d16cdf8bedc6fa2b44e24bc5410c
tonyzx0816 <lizhongxian23@gmail.com>
2026-04-06 09:33:51 -0700

    added a method called findByCartIdAndMenuItemId

--- src/main/java/com/laioffer/onlineorder/repository/OrderItemRepository.java
+++ src/main/java/com/laioffer/onlineorder/repository/OrderItemRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface OrderItemRepository extends ListCrudRepository<OrderItemEntity, Long> {
     List<OrderItemEntity> getAllByCartId(Long cartId);
+    OrderItemEntity findByCartIdAndMenuItemId(Long cartId, Long menuItemId);
     @Modifying
     @Query("DELETE FROM order_items WHERE cart_id = :cartId")
     void deleteByCartId(Long cartId);

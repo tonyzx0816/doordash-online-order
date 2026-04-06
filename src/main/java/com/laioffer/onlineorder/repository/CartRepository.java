7f12f106abc4874433a4c97d47a57d2319cb7b62
tonyzx0816 <lizhongxian23@gmail.com>
2026-04-06 09:19:24 -0700

    changed the function name

--- src/main/java/com/laioffer/onlineorder/repository/CartRepository.java
+++ src/main/java/com/laioffer/onlineorder/repository/CartRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CartRepository extends CrudRepository<CartEntity, Long> {
-    CartEntity findByCustomerId(Long customerId);
+    CartEntity getByCustomerId(Long customerId);
 
     @Modifying
     @Query("UPDATE carts SET total_price = :totalPrice WHERE id = :cartId")

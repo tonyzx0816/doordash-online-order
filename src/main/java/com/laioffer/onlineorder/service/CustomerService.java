29156eeee3a71644244614f5147c048641ca814a
tonyzx0816 <lizhongxian23@gmail.com>
2026-04-06 22:41:20 -0700

    implement customer service business logic

---
+++ src/main/java/com/laioffer/onlineorder/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.laioffer.onlineorder.service;
+
+import com.laioffer.onlineorder.entity.CustomerEntity;
+import com.laioffer.onlineorder.repository.CustomerRepository;
+
+public class CustomerService {
+   private final CustomerRepository customerRepository;
+
+   public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+   }
+
+   public CustomerEntity getCustomerByEmail(String email){
+       return customerRepository.findByEmail(email);
+   }
+}
